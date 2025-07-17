06b4dacd73341bcc3d9a0f132445eb3895af36fb
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-07-17 14:13:41 +0300

    feat(schema): add TokenBase model for parsing tokens in reset and verification flows

--- server/app/schemas/user.py
+++ server/app/schemas/user.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel, EmailStr, Field
 from typing import Optional, List
 import uuid
 
+class TokenBase(BaseModel):
+    token: str = Field(..., description="Access or reset token")
 
 class UserBase(BaseModel):
     username: str = Field(..., max_length=50)
@@ -10,15 +12,12 @@ class UserBase(BaseModel):
     class Config:
         from_attributes = True
 
-
 class UserCreate(UserBase):
     password: str = Field(..., min_length=6)
-
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
-
 
 class UserOut(BaseModel):
     id: uuid.UUID
@@ -36,7 +35,6 @@ class UserUpdate(BaseModel):
     username: Optional[str]
     password: Optional[str]
     avatar: Optional[str]
-
 
 class FullUser(UserOut):
     posts: Optional[List] = []

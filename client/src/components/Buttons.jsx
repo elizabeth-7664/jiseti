7e3620ad2af2050eb33849ad44ac9f298f18f3b9
Yvonne <ys359375@gmail.com>
2025-07-17 22:11:54 +0300

    final UI changes on existing  pages

--- client/src/components/Buttons.jsx
+++ client/src/components/Buttons.jsx
@@ -4,8 +4,7 @@ function Buttons({ button, onClick }) {
   return (
     <button
       onClick={onClick}
-      className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-200"
-    >
+      className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded shadow-md transition duration-200">
       {button}
     </button>
   );

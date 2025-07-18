4341b0e24ce076924a7ff21ad162804803642746
Lewis Robert Nakhungu <lokiyule@gmail.com>
2025-07-18 14:12:13 +0300

    update formhandling in Sign_in.jsx

--- client/src/pages/Sign_in.jsx
+++ client/src/pages/Sign_in.jsx
@@ -1,35 +1,59 @@
 import React, {useState} from "react";
 import './Sign_in.css'
+import API from "../utils/api";
+import {useNavigate, Link} from 'react-router-dom';
+import { useAuth } from "../context/AuthContext";
 
 function Sign_in(){
     const [email, setEmail]= useState("");
     const [password, setPassword] = useState("")
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const {login} = useAuth();
+    const navigate = useNavigate();
     
 
     async function handleSubmit(e){
         e.preventDefault();
+        setError(null);
+        setLoading(true);
+        try {
+            const formData =  new URLSearchParams();
+            formData.append('username', email);
+            formData.append('password', password);
 
-        if (password !== confirmPassword) {
-            alert('Passwords do not match');
-            return;
+            const res = await API.post('/api/login', formData, {
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded'},
+            });
+
+            login(res.data);
+            navigate('/dashboard');
+        }catch (err) {
+            console.error('Error:', err);
+            const errorMessage = err.response?.data?.detail || "Login failed.Please check your credintials."
+            setError(errorMessage);
+        }finally {
+            setLoading(false);
         }
-       
     };
 
     return(
+        
         <div className="container">
             <h2>Login to Jiseti</h2>
+               {error && <p className="bg-red-500/20 text-red-300 border border-red-500/30 p-3 rounded-lg mb-4 text-sm text-center">{error}</p>}
             <form onSubmit={handleSubmit} className="form">
                 <label>Email</label>
-                <input type="text" placeholder="e.g. example@gmail.com" value={email} onChange={(e)=> setEmail(e.target.value)} required />
+                <input type="email" placeholder="e.g. example@gmail.com" value={email} onChange={(e)=> setEmail(e.target.value)} required />
 
                 <label>Password</label>
-                <input type="text" placeholder="Enter your password" value={password} onChange={(e)=> setPassword(e.target.value)} />
+                <input type="password" placeholder="Enter your password" value={password} onChange={(e)=> setPassword(e.target.value)} />
 
-                <button type="submit">Sign In</button>
+                <button type="submit" disabled={loading}>{loading ? "Signing in..." : "Sign In"}
+                </button>
             </form>
             
-            <p className="redirect-text">Don't have an account? <a href="/sign_up">Sign Up</a></p>
+            <p className="redirect-text">Don't have an account? <Link to="/sign_up">Sign Up</Link></p>
         </div>
     )
 

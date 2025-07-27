17bbb00c683ae2377fd3c363f9f13f66c316f051
Lewis Robert Nakhungu <lokiyule@gmail.com>
2025-07-28 00:51:05 +0300

    add email verification page

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,7 +1,7 @@
 from .user import User
 from .report import Report
 from .comment import Comment
-from .notification import Notification
+#from .notification import Notification
 
 # Add others as you create them, e.g.:
 # from .role import Role
